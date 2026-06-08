9a527d17766912e18cc2867990977549e338aaae
2K <37275907+cuvotest@users.noreply.github.com>
2026-06-08 23:01:01 +0200

    Update ucb.ino

--- software/ucb.ino
+++ software/ucb.ino
@@ -1,24 +1,21 @@
 #include <ESP8266WiFi.h>
+
+#include "wifi.h"
 #include "webserver.h"
+#include "washingMachine.h"
 
-const char* ap_ssid = "UCB";
-const char* ap_password = "12345678";
+void setup()
+{
+    Serial.begin(115200);
 
-IPAddress local_IP(192, 168, 4, 1);
-IPAddress gateway(192, 168, 4, 1);
-IPAddress subnet(255, 255, 255, 0);
+    delay(500);
 
-void setup() {
-  Serial.begin(115200);
-  delay(500);
+    initWifi();
 
-  WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig(local_IP, gateway, subnet);
-  WiFi.softAP(ap_ssid, ap_password);
-
-  initWebServer();
+    initWebServer();
 }
 
-void loop() {
-  handleWebServer();
+void loop()
+{
+    handleWebServer();
 }
