0ec7f0eb1c64206fec2741227d2f268a1d253829
2K <37275907+cuvotest@users.noreply.github.com>
2026-06-08 22:59:53 +0200

    Refactor web server functions and add power controls

--- software/webserver.cpp
+++ software/webserver.cpp
@@ -1,21 +1,76 @@
 #include <ESP8266WebServer.h>
-#include "ui.h"
+
 #include "webserver.h"
+#include "ui.h"
+#include "washingMachine.h"
 
 ESP8266WebServer server(80);
 
-void initWebServer() {
-  server.on("/", HTTP_GET, []() {
+void handleRoot()
+{
     server.setContentLength(CONTENT_LENGTH_UNKNOWN);
-    server.send(200, "text/html", "");
+
+    server.send(
+        200,
+        "text/html",
+        ""
+    );
+
     server.sendContent_P(pageTop);
     server.sendContent_P(logoSvg);
     server.sendContent_P(pageBottom);
-  });
-
-  server.begin();
 }
 
-void handleWebServer() {
-  server.handleClient();
+void handlePowerOn()
+{
+    powerOn();
+
+    server.send(
+        200,
+        "text/plain",
+        "OK"
+    );
+}
+
+void handlePowerOff()
+{
+    powerOff();
+
+    server.send(
+        200,
+        "text/plain",
+        "OK"
+    );
+}
+
+void initWebServer()
+{
+    server.on(
+        "/",
+        HTTP_GET,
+        handleRoot
+    );
+
+    server.on(
+        "/on",
+        HTTP_GET,
+        handlePowerOn
+    );
+
+    server.on(
+        "/off",
+        HTTP_GET,
+        handlePowerOff
+    );
+
+    server.begin();
+
+    Serial.println(
+        "Webserver gestartet"
+    );
+}
+
+void handleWebServer()
+{
+    server.handleClient();
 }
