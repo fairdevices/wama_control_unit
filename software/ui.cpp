b3bfeea8fbd5480a1f5ea6750f5d5a5b10772d74
2K <37275907+cuvotest@users.noreply.github.com>
2026-06-08 22:58:54 +0200

    Add UI elements to ui.cpp
    Added UI elements including pageTop, logoSvg, and pageBottom.

--- software/ui.cpp
+++ software/ui.cpp
@@ -1,1 +1,13 @@
-ui.cpp
+#include "ui.h"
+
+const char pageTop[] PROGMEM = R"rawliteral(
+DEIN GESAMTER pageTop BLOCK
+)rawliteral";
+
+const char logoSvg[] PROGMEM = R"rawliteral(
+DEIN GESAMTER logoSvg BLOCK
+)rawliteral";
+
+const char pageBottom[] PROGMEM = R"rawliteral(
+DEIN GESAMTER pageBottom BLOCK
+)rawliteral";ui.cpp
