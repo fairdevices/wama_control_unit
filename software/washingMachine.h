10123252e1082cb361724465f33d507634c72eab
2K <37275907+cuvotest@users.noreply.github.com>
2026-06-08 22:57:23 +0200

    Define WashingMachineState structure and functions

--- software/washingMachine.h
+++ software/washingMachine.h
@@ -1,1 +1,25 @@
-washingMachine.h
+#pragma once
+
+struct WashingMachineState
+{
+    bool power;
+
+    String program;
+    String temperature;
+    String spinningSpeed;
+
+    bool prewash;
+    bool extraRinse;
+};
+
+extern WashingMachineState machine;
+
+void powerOn();
+void powerOff();
+
+void setProgram(String value);
+void setTemperature(String value);
+void setSpinningSpeed(String value);
+
+void setPrewash(bool value);
+void setExtraRinse(bool value);
