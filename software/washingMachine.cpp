0398297f49a7002824eade4114191644a8a0d46e
2K <37275907+cuvotest@users.noreply.github.com>
2026-06-08 22:58:01 +0200

    Implement washing machine control functions

--- software/washingMachine.cpp
+++ software/washingMachine.cpp
@@ -1,1 +1,46 @@
-washingMachine.cpp
+#include "washingMachine.h"
+
+WashingMachineState machine =
+{
+    false,
+    "",
+    "",
+    "",
+    false,
+    false
+};
+
+void powerOn()
+{
+    machine.power = true;
+}
+
+void powerOff()
+{
+    machine.power = false;
+}
+
+void setProgram(String value)
+{
+    machine.program = value;
+}
+
+void setTemperature(String value)
+{
+    machine.temperature = value;
+}
+
+void setSpinningSpeed(String value)
+{
+    machine.spinningSpeed = value;
+}
+
+void setPrewash(bool value)
+{
+    machine.prewash = value;
+}
+
+void setExtraRinse(bool value)
+{
+    machine.extraRinse = value;
+}
