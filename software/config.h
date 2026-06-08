b6d83faac013c9c4748ff6d62b6f0561fde2996b
2K <37275907+cuvotest@users.noreply.github.com>
2026-06-08 22:55:53 +0200

    Update config.h

--- software/config.h
+++ software/config.h
@@ -1,1 +1,10 @@
-config.h
+#pragma once
+
+#include <ESP8266WiFi.h>
+
+const char* const AP_SSID = "UCB";
+const char* const AP_PASSWORD = "12345678";
+
+const IPAddress LOCAL_IP(192,168,4,1);
+const IPAddress GATEWAY(192,168,4,1);
+const IPAddress SUBNET(255,255,255,0);
