a507fdce89aeca6e40da67a8d2ca9f65d7016bde
2K <37275907+cuvotest@users.noreply.github.com>
2026-06-10 20:24:33 +0200

    Refactor washingMachine.h with header guards
    Added header guards and expanded WashingMachineState struct.

--- software/washingMachine.h
+++ software/washingMachine.h
@@ -1,4 +1,26 @@
-#pragma once
+#ifndef WASHING_MACHINE_H
+#define WASHING_MACHINE_H
+
+#include <Arduino.h>
+
+struct WashingMachineState {
+  bool powerOn;
+  bool prewash;
+  bool extraRinsing;
+  String program;
+  String temperature;
+  String spinningSpeed;
+};
+
+void washingMachineInit();
+void setPower(bool enabled);
+bool isPowerOn();
+
+WashingMachineState getWashingMachineState();
+void setWashingMachineState(const WashingMachineState& newState);
+String washingMachineStateJson();
+
+#endif#pragma once
 
 struct WashingMachineState
 {
