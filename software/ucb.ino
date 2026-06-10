a4af3651390b2e0138c35dea6ceef18c19905bd4
2K <37275907+cuvotest@users.noreply.github.com>
2026-06-10 20:22:00 +0200

    Reorganize setup and loop functions in ucb.ino

--- software/ucb.ino
+++ software/ucb.ino
@@ -1,21 +1,16 @@
-#include <ESP8266WiFi.h>
-
-#include "wifi.h"
-#include "webserver.h"
 #include "washingMachine.h"
+#include "webserver.h"
+#include "wifi.h"
 
-void setup()
-{
-    Serial.begin(115200);
+void setup() {
+  Serial.begin(115200);
+  delay(500);
 
-    delay(500);
-
-    initWifi();
-
-    initWebServer();
+  washingMachineInit();
+  setupAccessPoint();
+  initWebServer();
 }
 
-void loop()
-{
-    handleWebServer();
+void loop() {
+  handleWebServerClient();
 }
