6c42817c4a7db4decd2ebf7178caa94488859bb4
2K <37275907+cuvotest@users.noreply.github.com>
2026-06-10 20:23:33 +0200

    Add include guards to ui.h

--- software/ui.h
+++ software/ui.h
@@ -1,6 +1,10 @@
-#pragma once
+#ifndef UI_H
+#define UI_H
+
 #include <Arduino.h>
 
 extern const char pageTop[] PROGMEM;
 extern const char logoSvg[] PROGMEM;
 extern const char pageBottom[] PROGMEM;
+
+#endif
