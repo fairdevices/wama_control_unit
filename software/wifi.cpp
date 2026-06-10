d06ddb033d726a423e422803943f5559fe09c7c0
2K <37275907+cuvotest@users.noreply.github.com>
2026-06-10 20:26:01 +0200

    Update wifi.cpp

--- software/wifi.cpp
+++ software/wifi.cpp
@@ -1,24 +1,15 @@
-#include <ESP8266WiFi.h>
-#include "config.h"
 #include "wifi.h"
 
-void initWifi()
-{
-    WiFi.mode(WIFI_AP);
+#include <ESP8266WiFi.h>
+#include "config.h"
 
-    WiFi.softAPConfig(
-        LOCAL_IP,
-        GATEWAY,
-        SUBNET
-    );
+void setupAccessPoint() {
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(LOCAL_IP, GATEWAY, SUBNET);
+  WiFi.softAP(AP_SSID, AP_PASSWORD);
 
-    WiFi.softAP(
-        AP_SSID,
-        AP_PASSWORD
-    );
-
-    Serial.println();
-    Serial.println("Access Point gestartet");
-    Serial.print("IP-Adresse: ");
-    Serial.println(WiFi.softAPIP());
-}wifi.cpp
+  Serial.println();
+  Serial.println("Access Point gestartet");
+  Serial.print("IP-Adresse: ");
+  Serial.println(WiFi.softAPIP());
+}
