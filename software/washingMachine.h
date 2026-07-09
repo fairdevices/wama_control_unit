96824682c73f14253f10bf08e8333c1f2c5f652f
2K <37275907+cuvotest@users.noreply.github.com>
2026-07-09 23:50:40 +0200

    Remove WashingMachineState struct and related functions
    Removed the definition of the WashingMachineState struct and related function declarations.

--- software/washingMachine.h
+++ software/washingMachine.h
@@ -20,28 +20,4 @@ WashingMachineState getWashingMachineState();
 void setWashingMachineState(const WashingMachineState& newState);
 String washingMachineStateJson();
 
-#endif#pragma once
-
-struct WashingMachineState
-{
-    bool power;
-
-    String program;
-    String temperature;
-    String spinningSpeed;
-
-    bool prewash;
-    bool extraRinse;
-};
-
-extern WashingMachineState machine;
-
-void powerOn();
-void powerOff();
-
-void setProgram(String value);
-void setTemperature(String value);
-void setSpinningSpeed(String value);
-
-void setPrewash(bool value);
-void setExtraRinse(bool value);
+#endif
